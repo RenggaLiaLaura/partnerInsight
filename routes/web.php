7eeba977266bbbceacda49f7db6d9f28136b9dc4
kdpras00 <kdpras00@gmail.com>
2025-11-29 17:46:23 +0700

    fix page import distributor

--- routes/web.php
+++ routes/web.php
@@ -41,13 +41,14 @@ Route::middleware(['auth'])->group(function () {
     // Manager & Admin Routes - Read-only access to distributors
     Route::middleware(['role:manager,admin'])->group(function () {
         Route::get('/distributors', [DistributorController::class, 'index'])->name('distributors.index');
-        Route::get('/distributors/{distributor}', [DistributorController::class, 'show'])->name('distributors.show');
         
-        // Import/Export routes (Manager & Admin)
+        // Import/Export routes (Manager & Admin) - MUST be before {distributor} wildcard
         Route::get('/distributors/import', [DistributorController::class, 'showImportForm'])->name('distributors.import');
         Route::post('/distributors/import', [DistributorController::class, 'import'])->name('distributors.import.process');
         Route::get('/distributors/template', [DistributorController::class, 'downloadTemplate'])->name('distributors.template');
         Route::get('/export-all-data', [DistributorController::class, 'exportAll'])->name('export.all');
+
+        Route::get('/distributors/{distributor}', [DistributorController::class, 'show'])->name('distributors.show');
         
         // Clustering routes
         Route::get('/clustering', [ClusteringController::class, 'index'])->name('clustering.index');
